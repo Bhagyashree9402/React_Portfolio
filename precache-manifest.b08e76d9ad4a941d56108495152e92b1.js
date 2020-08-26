dea140d96587d8465dc0f9284f7e0e8433495d0b
unknown <bhagyashree9402@gmail.com>
2020-08-26 01:48:27 -0700

    Updates

---
+++ precache-manifest.b08e76d9ad4a941d56108495152e92b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b427a2ccb51883b52f5e343fdc7192c",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "d63fb899bd1347c4010f",
+    "url": "/React_Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9239432ae51996d1df7d",
+    "url": "/React_Portfolio/static/js/2.f164797f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Portfolio/static/js/2.f164797f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63fb899bd1347c4010f",
+    "url": "/React_Portfolio/static/js/main.11c5b1ce.chunk.js"
+  },
+  {
+    "revision": "05fe8615c155b9c6e394",
+    "url": "/React_Portfolio/static/js/runtime-main.f4efdfd9.js"
+  }
+]);
