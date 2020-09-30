c0290cdff7328686452af859f61df60891b39705
Bhagyashree <bhagyashree9402@gmail.com>
2020-09-29 21:35:39 -0700

    minor changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,142 +1,142 @@
-import React ,{useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import MobileRightMenuSlider from "@material-ui/core/Drawer"
 import Footer from "./Footer"
 
-import { 
-AppBar,
-ToolBar,
-ListItem,
-IconButton,
-ListItemText,
-Divider,
-List,
-Typography,
-Box,
-Toolbar,
-Avatar,
-ListItemIcon,
+import {
+    AppBar,
+    ToolBar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Divider,
+    List,
+    Typography,
+    Box,
+    Toolbar,
+    Avatar,
+    ListItemIcon,
 } from "@material-ui/core"
-import{ 
+import {
     ArrowBack,
     AssignmentInd,
     Home,
     Apps,
     ContactMail
 }
-from "@material-ui/icons"
+    from "@material-ui/icons"
 import avatar from "./avatar.png"
 
 //CSS styles
-const useStyles = makeStyles(theme=>(
+const useStyles = makeStyles(theme => (
     {
-        menuSliderContainer:{
-            width:250,
-            background:"#511",
-            height:"100%" 
+        menuSliderContainer: {
+            width: 250,
+            background: "#511",
+            height: "100%"
         },
-        avatar : {
+        avatar: {
             display: "block",
-            margin:"0.5rem auto",
-            width:theme.spacing(13),
-            height:theme.spacing(13)
+            margin: "0.5rem auto",
+            width: theme.spacing(13),
+            height: theme.spacing(13)
         },
-        listItem:{
-            color:"tan"
+        listItem: {
+            color: "tan"
         }
     }
 ))
 
-const menuItems=[
+const menuItems = [
     {
-        listIcon:<Home/>,
-        listText:"Home",
-        listPath:"/"
-       
+        listIcon: <Home />,
+        listText: "Home",
+        listPath: "/"
+
     },
     {
-        listIcon:<AssignmentInd/>,
-        listText:"Resume",
-        listPath:"/resume"
-      
+        listIcon: <AssignmentInd />,
+        listText: "Resume",
+        listPath: "/resume"
+
     },
     {
-        listIcon:<Apps/>,
-        listText:"Portfolio",
-        listPath:"/portfolio"
+        listIcon: <Apps />,
+        listText: "Portfolio",
+        listPath: "/portfolio"
     },
     {
-        listIcon:<ContactMail/>,
-        listText:"Contacts",
-        listPath:"/contacts"
+        listIcon: <ContactMail />,
+        listText: "Contact",
+        listPath: "/contacts"
     }
-    
+
 ]
 
 
 
 const Nav = () => {
-    const [state,setState] = useState({
-        right:false
+    const [state, setState] = useState({
+        right: false
     });
 
-    const toggleSlider=((slider,open) => ()=>{
-        setState({...state,[slider]:open});
+    const toggleSlider = ((slider, open) => () => {
+        setState({ ...state, [slider]: open });
     });
 
 
-    const classes =useStyles();
+    const classes = useStyles();
 
-    const sideList=slider=>(
-        <Box  className={classes.menuSliderContainer} 
-        component="div"
-        onClick={toggleSlider(slider,false)}
+    const sideList = slider => (
+        <Box className={classes.menuSliderContainer}
+            component="div"
+            onClick={toggleSlider(slider, false)}
         >
-            <Avatar className={classes.avatar} src={avatar} alt="not working"/>
-            <Divider/>
+            <Avatar className={classes.avatar} src={avatar} alt="not working" />
+            <Divider />
             <List>
-                {menuItems.map((lsItem,key)=>(
-                   <ListItem button key={key} component={Link} to={lsItem.listPath}>
-                   <ListItemIcon className={classes.listItem}>
-                     {lsItem.listIcon}
-                   </ListItemIcon>
-                   <ListItemText className={classes.listItem} primary={lsItem.listText}/>
-                  </ListItem>
+                {menuItems.map((lsItem, key) => (
+                    <ListItem button key={key} component={Link} to={lsItem.listPath}>
+                        <ListItemIcon className={classes.listItem}>
+                            {lsItem.listIcon}
+                        </ListItemIcon>
+                        <ListItemText className={classes.listItem} primary={lsItem.listText} />
+                    </ListItem>
                 )
                 )
                 }
-               
+
             </List>
 
         </Box>
 
     )
-    
+
     return (
         <>
-        
-        <Box component="nav">
-           <AppBar position="static" style={{background:"#222"}}>
-               <Toolbar>
-                   <IconButton onClick={toggleSlider("right",true)}>
-                   <ArrowBack style={{color:"tomato"}}/>
-                   </IconButton>
 
-                 <Typography variant="h5" style={{color:"tan"}}>
-                   Portfolio
+            <Box component="nav">
+                <AppBar position="static" style={{ background: "#222" }}>
+                    <Toolbar>
+                        <IconButton onClick={toggleSlider("right", true)}>
+                            <ArrowBack style={{ color: "tomato" }} />
+                        </IconButton>
+
+                        <Typography variant="h5" style={{ color: "tan" }}>
+                            Portfolio
                  </Typography>
-                 <MobileRightMenuSlider
-                 anchor="right"
-                 open={state.right}
-                 onClose={toggleSlider("right",false)}
-                 >
-                  {sideList("right")}
-                  <Footer/>
-                 </MobileRightMenuSlider>
-               </Toolbar>
-           </AppBar>
-        </Box>
+                        <MobileRightMenuSlider
+                            anchor="right"
+                            open={state.right}
+                            onClose={toggleSlider("right", false)}
+                        >
+                            {sideList("right")}
+                            <Footer />
+                        </MobileRightMenuSlider>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         </>
     )
 }
